226917852645fa410d6a7ec4f8dae0f5eefa6525
lichain <airy.cloud@gmail.com>
2014-08-07 05:13:31 +0800

    commit fib.s

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -15,18 +15,29 @@ fibonacci:
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs R4, R0, #0
+	cmp R4, #1
+	ble .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp R4, #1
+	beq .L4
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
+	subs R0, R4, #1
+	bl fibonacci
 
 	@ R5 = R0
 	@ R0 = R4 - 2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
+	mov R5, R0
+	subs R0, R4, #2
+	bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
+	adds R0, R5, R0
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
@@ -34,10 +45,8 @@ fibonacci:
 .L3:
 	mov r0, #0			@ R0 = 0
 	pop {r3, r4, r5, pc}		@ EPILOG
-
 .L4:
 	mov r0, #1			@ R0 = 1
 	pop {r3, r4, r5, pc}		@ EPILOG
-
 	.size fibonacci, .-fibonacci
 	.end
